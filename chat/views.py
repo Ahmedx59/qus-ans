5a75d75c262cf10f4ba80a2ad2fb0a5ee0a58bcd
Ahmedx59 <Ahmedx59@gmail.com>
2024-03-15 15:17:55 +0200

    add question list view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Question,Answers 
 
 
+def question_list(request):
+    question = Question.objects.all()
+    return render(request,'question.html',{question:'que'})
